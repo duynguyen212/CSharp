b2b9a4f52782de3d74568749c07ae7570553ca58
duynguyen212 <duynguyen212@gmail.com>
2017-04-26 14:00:23 +0200

    Simple Calculation in Console

---
+++ SimpleCalculation/SimpleCalculation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monPrenom;
+            double nombre1, nombre2;
+
+            Console.Write("Entrez votre prénom: ");
+            monPrenom = Console.ReadLine();
+            Console.WriteLine($"Bonjour, {monPrenom}");
+
+            Console.Write("Entrez premier nombre: ");
+            nombre1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Entrez deuxième nombre: ");
+            nombre2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{nombre1} + {nombre2} = " + $"{nombre1 + nombre2}");
+            Console.WriteLine($"{nombre1} - {nombre2} = " + $"{nombre1 - nombre2}");
+            Console.WriteLine($"{nombre1} * {nombre2} = " + $"{nombre1 * nombre2}");
+            Console.WriteLine($"{nombre1} / {nombre2} = " + $"{nombre1 / nombre2}");
+            Console.WriteLine($"{nombre1} % {nombre2} = " + $"{nombre1 % nombre2}");
+
+            Console.ReadKey();
+        }
+    }
+}
+

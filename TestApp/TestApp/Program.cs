83f78fa80898e460f0416404def9f49e71f99713
duynguyen212 <duynguyen212@gmail.com>
2017-04-29 17:37:39 +0200

    adding one command to test pushing GIT

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -11,6 +11,7 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Testing to Push to GitHub");
             Console.ReadKey();
         }
     }
